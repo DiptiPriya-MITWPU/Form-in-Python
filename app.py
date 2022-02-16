cda25c6a0a26b92cee86d0cb7d2f774d3c93c7d4
Aman Jee <amanjee@Amans-MacBook-Air.local>
2022-02-16 10:52:32 +0530

    Form create

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__, template_folder='template')
+
+subscribers = []
 
 @app.route("/")
 def index():
@@ -9,3 +11,17 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/subscribe")
+def subscribe():
+    return render_template("subscribe.html")
+
+@app.route("/form", methods=["POST"])
+def form():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+
+    subscribers.append(f"{first_name} {last_name} | {email}")
+
+    return render_template("form.html", subscribers=subscribers)
